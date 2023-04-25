fa6f90cb0d4d2dd1587aab086a7dc628b076e585
Shashank <shashankofficial3209@gmail.com>
2023-04-25 09:12:16 +0530

    Post-Registration-Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require('express');
 
 const app=express();
+const bcryptjs=require('bcryptjs')
 
 require('./db/connection');
 
@@ -15,13 +16,25 @@ app.use(express.urlencoded({extended: false}));
 app.get('/',(req,res)=>{
     res.send("Belcome Guys")
 })  
-app.post('api/register',async(req,res)=>{
+app.post('/api/register',async(req,res,next)=>{
     try{
         const {fullName,email,password}=req.body;
         if(!fullName|| !email||!password){
             res.status(400).send('please fill all the required fields')
         }else{
             const isAlreadyExist= await Users.findOne({email})
+
+            if(isAlreadyExist){
+                res.status(300).send("user Already exist")
+            }else{
+                const newUser=new Users({fullName,email})
+                bcryptjs.hash(password,10,(err,hashedPassword)=>{
+                    newUser.set('password',hashedPassword);
+                    newUser.save()
+                    next();
+                })
+                return res.status(200).send("User Registered Sucessfully")
+            }
         }
 
     }catch(error){
